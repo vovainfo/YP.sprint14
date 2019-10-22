c22b61515e95ff91e8f76611f223de416c5a9d09
Vladimir Gumennikov <>
2019-10-22 13:36:45 +1000

    на второе ревью

--- controllers/cards.js
+++ controllers/cards.js
@@ -8,7 +8,7 @@ module.exports.getAllCards = (req, res) => {
 
 module.exports.postCard = (req, res) => {
   const { name, link } = req.body;
-  Card.create({ name, link })
+  Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send(card))
     .catch((err) => res.status(500).send(err));
 };
